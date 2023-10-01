9b5f619e1e5ba1c8266fd44a6d84c299973db1aa
bartz-marcelo <marcelo.bartz1971@gmail.com>
2023-10-01 20:58:52 -0300

    Exercício da aula 03.

---
+++ Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        double nota1, nota2, media;
+        final double PESO_NOTA = 0.5;
+        nota1 = 7;
+        nota2 = 10;
+        media = (nota1 * PESO_NOTA) + (nota2 * PESO_NOTA);
+        System.out.println("O valor da média das duas notas é " + media + ".");
+
+    }
+}
